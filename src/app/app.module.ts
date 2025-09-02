c6993bcb8ac9b1ee099170d016795edde368e8fd
Tyler Lin <ddkc747051@gmail.com>
2025-09-02 15:17:44 +0800

    移除未使用的 EmptyRouteComponent 引入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [AppComponent, LayoutComponent],
